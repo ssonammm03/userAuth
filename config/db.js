84a799eb70a18b48177917e19b10891ed19279df
Sonam03 <07220020.sherubtse@rub.edu.bt>
2025-06-26 10:53:58 +0600

    Initial commit

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-
 const pgpInit = require('pg-promise')
 require('dotenv').config();
 
@@ -7,15 +6,14 @@ const pgp = pgpInit();
 
 
 const db = pgp({
-    host: 'localhost',
-    port: process.env.DB_PORT,
+    host:  process.env.DB_HOST,
+    port: 5432,     // default port
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     ssl: {
-        rejectUnauthorized: false // Set to true if you have a valid SSL certificate
+        rejectUnauthorized: false, //set to true if you want to reject unauthorized SSL certificates
     }
 })
 
-
-module.exports = db;
+module.exports = db;
